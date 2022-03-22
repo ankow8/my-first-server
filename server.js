48dcd05623c9293c3495f1c28025709a9dfb4441
Antoni Kowalewski <kowalewski.antek@gmail.com>
2022-03-22 22:31:35 +0100

    Add heroku var

--- server.js
+++ server.js
@@ -38,9 +38,9 @@ app.use((req,res) => {
 const NODE_ENV = process.env.NODE_ENV;
 let dbUri = '';
 
-if(NODE_ENV === 'production') dbUri = 'mongodb+srv://ankow8:haslo1234@cluster0.0d0yr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+if(NODE_ENV === 'production') dbUri = `mongodb+srv://${process.env.TEST_LOGIN}:${process.env.TEST_PASSWORD}@cluster0.0d0yr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 else if(NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/NewWaveDBTest';
-else dbUri = 'mongodb+srv://ankow8:haslo1234@cluster0.0d0yr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+else dbUri = `mongodb+srv://${process.env.TEST_LOGIN}:${process.env.TEST_PASSWORD}@cluster0.0d0yr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
